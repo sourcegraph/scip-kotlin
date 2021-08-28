b9ada8b044ea7b65ed7b2358a68c10b9241d1d85
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-08-28 13:33:58 +0100

    artifact signing fix attempt #2

--- semanticdb-kotlinc/build.gradle.kts
+++ semanticdb-kotlinc/build.gradle.kts
@@ -112,7 +112,9 @@ publishing {
 }
 
 signing {
-    useGpgCmd()
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["shadow"])
 }
 
